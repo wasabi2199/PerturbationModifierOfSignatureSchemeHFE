7c2ac5256e00389a3cb38b690c611d8414166f5e
wasabi2199 <75641027+wasabi2199@users.noreply.github.com>
2023-03-20 16:51:57 +0100

    konzultacia 7

--- NTL_APP/Signature.h
+++ NTL_APP/Signature.h
@@ -2,12 +2,16 @@
 
 #include "Polynomial.h"
 #include "AffineTransformation.h"
+#include <bitset>
 
 namespace Signature {
 	template <class T>
-	Vec<T> generateSignature(const GF2EX& hfe,const Mat<T>& matrix_T, const Vec<T>& vector_T, const Mat<T>& matrix_S, const Vec<T>& vector_S, const Vec<GF2>& y, long modulus_deg) {
+	Vec<T> generateSignature(long a,  const GF2EX& hfe, const Mat<T>& matrix_T, const Vec<T>& vector_T, const Mat<T>& matrix_S, const Vec<T>& vector_S, const Vec<GF2>& y_without_a, long modulus_deg) {
 		Vec<GF2> hodnoty_TP;
-
+		Vec<GF2> y = y_without_a;
+		for (int i = 0; i < a; i++) {
+			y.append(random_GF2());
+		}
 		//inverzna transformacia ku T na vektor pre spravu (vektor y)
 		hodnoty_TP = AffineTransformation::applyInverseAffineTransformation(y, matrix_S, vector_S);
 
@@ -38,15 +42,15 @@ namespace Signature {
 		return x;
 	}
 
-	bool verifySignature(const Vec<GF2>& signature, const Vec<GF2>& message, const Vec<Polynomial<GF2>>& public_key, long modulus_deg) {
+	bool verifySignature(long a, const Vec<GF2>& signature, const Vec<GF2>& message, const Vec<Polynomial<GF2>>& public_key, long modulus_deg) {
 		Vec<GF2> PK_values;
-		for (int i = 0; i < modulus_deg; i++) {
+		for (int i = 0; i < modulus_deg - a; i++) {
 			PK_values.append(public_key[i].getConstant());
 			PK_values[i] += public_key[i].getLinearCoefficient() * signature + signature * public_key[i].getQuadraticCoefficient() * signature;
 		}
 
 		cout << "PK_values " << PK_values << endl;
-
+		cout << "message " << message << endl;
 		if (message == PK_values) {
 			return true;
 		}
@@ -54,20 +58,32 @@ namespace Signature {
 	}
 
 	template <class T>
-	Vec<GF2> generateSignaturePerturbed(const Mat<T>& matrix_T, const Vec<T>& vector_T, const Mat<T>& matrix_S, const Vec<T>& vector_S, const GF2EX& hfe, const Vec<Mat<GF2>>& perturbation_polynomials, const Vec<GF2E>& betas, const Vec<GF2>& y, long modulus_deg, long t) {
+	Vec<GF2> generateSignaturePerturbed(long a, const Mat<T>& matrix_T, const Vec<T>& vector_T, const Mat<T>& matrix_S, const Vec<T>& vector_S, const GF2EX& hfe, const Vec<Mat<GF2>>& perturbation_polynomials, const Vec<GF2E>& betas, const Vec<GF2>& y_without_a, long modulus_deg, long const t) {
 		Vec<GF2> hodnoty_TP;
-
+		Vec<GF2> y = y_without_a;
+		for (int i = 0; i < a; i++) {
+			y.append(random_GF2());
+		}
 		//inverzna transformacia ku T na vektor pre spravu (vektor y)
 		hodnoty_TP = AffineTransformation::applyInverseAffineTransformation(y, matrix_S, vector_S);
 		bool root_exists = false;
 		GF2E X;
 		Vec<GF2> pert_root;
 		Vec<GF2> X_vec;
-		while (1) {
-			Vec<GF2> t_values;
-			for (int i = 0; i < t; i++) {
-				t_values.append(random_GF2());
+		//long const tt = 4;
+		//ak by t bolo const
+		Vec<Vec<GF2>> vektory2;
+		for (int i = 0; i < pow(2, t); i++) {
+			Vec<GF2> bit_vec_GF2;
+			std::bitset<2> bit_vec(i);
+			for (int j = 0; j < t; j++) {
+				bit_vec_GF2.append(GF2(bit_vec[j]));
 			}
+			//bit_vec_GF2.SetLength(modulus_deg);
+			vektory2.append(bit_vec_GF2);
+		}
+		for (Vec<GF2> t_values : vektory2) {
+			cout << "t values " << t_values << endl;
 			GF2EX hfe_betas = hfe;
 			for (int i = 0; i < t; i++) {
 				hfe_betas += betas[i] * t_values[i];
@@ -81,7 +97,6 @@ namespace Signature {
 			for (auto c : roots) {
 				if (deg(c.a) == 1) {
 					X = ConstTerm(c.a); //X je koren
-					root_exists = true;
 					//overenie ci je koren platne riesenie perturbacnych polynomov
 					X_vec = conv<Vec<GF2>>(conv<GF2X>(X));
 					X_vec.SetLength(modulus_deg);
@@ -98,11 +113,90 @@ namespace Signature {
 				break;
 			}
 		}
+		if (root_exists == false) {
+			cout << "nenaslo sa riesenie pri inverzii " << endl;
+			return Vec<GF2>();
+		}
 		//invertovanie vektora x podla transformacie T (x = platny podpis)
 		Vec<GF2> x = AffineTransformation::applyInverseAffineTransformation(X_vec, matrix_T, vector_T);
 
 		return x;
 		
+	}
+
+	template <class T>
+	Vec<GF2> generateSignaturePerturbedP(long a, Vec<Polynomial<GF2>>pert_sys_of_polynomials, const Mat<T>& matrix_T, const Vec<T>& vector_T, const Mat<T>& matrix_S, const Vec<T>& vector_S, const GF2EX& hfe, const Vec<Mat<GF2>>& perturbation_polynomials, const Vec<GF2E>& betas, const Vec<GF2>& y_without_a, long modulus_deg, long const t) {
+		Vec<GF2> hodnoty_TP;
+		Vec<GF2> y = y_without_a;
+		for (int i = 0; i < a; i++) {
+			y.append(random_GF2());
+		}
+		//inverzna transformacia ku T na vektor pre spravu (vektor y)
+		hodnoty_TP = AffineTransformation::applyInverseAffineTransformation(y, matrix_S, vector_S);
+		bool root_exists = false;
+		GF2E X;
+		Vec<GF2> pert_root;
+		Vec<GF2> X_vec;
+		//long const tt = 4;
+		//ak by t bolo const
+		Vec<Vec<GF2>> vektory2;
+		Vec<GF2E> betas_linear;
+		for (int i = 0; i < pow(2, t); i++) {
+			Vec<GF2> bit_vec_GF2;
+			std::bitset<2> bit_vec(i);
+			GF2E temp = GF2E();
+			for (int j = 0; j < t; j++) {
+				bit_vec_GF2.append(GF2(bit_vec[j]));
+				temp += bit_vec_GF2[j] * betas[j];
+			}
+			//bit_vec_GF2.SetLength(modulus_deg);
+			vektory2.append(bit_vec_GF2);
+			betas_linear.append(temp);
+		}
+		GF2EX projection_poly;
+		BuildFromRoots(projection_poly, betas_linear);
+		cerr << "proj poly: " << projection_poly << endl;
+		GF2E hodnoty_TP_projection = eval(projection_poly, conv<GF2E>(conv<GF2X>(hodnoty_TP)));
+		GF2EX hfe_projection = GF2EX();
+		cerr << hodnoty_TP_projection << endl;
+		for (long i = 0; i <= deg(projection_poly); i++) {
+			if (!IsZero(coeff(projection_poly, i))) {
+				hfe_projection += coeff(projection_poly, i) * power(hfe, i);
+			}
+		}
+		cerr << "proj hfe: " << hfe_projection << endl;
+		hfe_projection -= hodnoty_TP_projection;
+		MakeMonic(hfe_projection);
+		Vec<Pair<GF2EX, long>> roots;
+		roots = berlekamp(hfe_projection);
+		cerr << roots << endl;
+		pert_root.SetLength(modulus_deg);
+		for (auto c : roots) {
+			if (deg(c.a) == 1) {
+				X = ConstTerm(c.a); //X je koren
+				//overenie ci je koren platne riesenie perturbacnych polynomov
+				X_vec = conv<Vec<GF2>>(conv<GF2X>(X));
+				X_vec.SetLength(modulus_deg);
+				for (int i = 0; i < modulus_deg; i++) {
+					pert_root[i] = X_vec * pert_sys_of_polynomials[i].getQuadraticCoefficient() * X_vec;
+					pert_root[i] += X_vec * pert_sys_of_polynomials[i].getLinearCoefficient();
+					pert_root[i] += pert_sys_of_polynomials[i].getConstant();
+				}
+				if (pert_root == hodnoty_TP) {
+					root_exists = true;
+					break;
+				}
+			}
+		}
+		if (root_exists == false) {
+			cout << "nema riesenie" << endl;
+			return Vec<GF2>();
+		}
+
+		//invertovanie vektora x podla transformacie T (x = platny podpis)
+		Vec<GF2> x = AffineTransformation::applyInverseAffineTransformation(X_vec, matrix_T, vector_T);
+		return x;
+
 	}
 
 }
--- NTL_APP/Source.cpp
+++ NTL_APP/Source.cpp
@@ -16,6 +16,7 @@
 #include "NTL/GF2EXFactoring.h"
 #include "Signature.h"
 #include "PublicKey.h"
+#include <bitset>
 
 NTL_CLIENT
 
@@ -30,12 +31,12 @@ int main()
 	//cout << "zadaj modulus polynom> ";
 	//cin >> modulus;
 	//GF2E::init(modulus);
-
 	//SetSeed(conv<ZZ>(1));
 
 	long modulus_deg = 3; //= deg(modulus);
 	long hfe_deg = 7;
-	long t = 2;
+	long const t = 2;
+	long a = 1;
 	GF2EX hfe = HFE::generateHFEPolynomial(modulus_deg, hfe_deg);
 
 	Vec<Polynomial<GF2>> sustava_polynomov = HFE :: hfeToSystemOfPolynomials(modulus_deg, hfe);
@@ -48,13 +49,13 @@ int main()
 	
 	Vec<Polynomial<GF2>> public_key = PublicKey::getPublicKey(matrix_T, vector_T, matrix_S, vector_S, sustava_polynomov);
 
-	Vec<GF2> message = random_vec_GF2(modulus_deg);
-	Vec<GF2> signature = Signature::generateSignature(hfe, matrix_T, vector_T, matrix_S, vector_S, message, modulus_deg);
+	Vec<GF2> message = random_vec_GF2(modulus_deg - a);
+	Vec<GF2> signature = Signature::generateSignature(a, hfe, matrix_T, vector_T, matrix_S, vector_S, message, modulus_deg);
 
 	cout << "message " << message << endl;
 	cout << "signature " << signature << endl;
 
-	cout << Signature::verifySignature(signature, message, public_key, modulus_deg) << endl;
+	cout << Signature::verifySignature(a, signature, message, public_key, modulus_deg) << endl;
 
 	Vec<GF2E> betas;
 	Vec<Mat<GF2>> perturbation_polynomials;
@@ -64,10 +65,14 @@ int main()
 	cout << "pert_sys_of_polynomials " << pert_sys_of_polynomials << endl;
 
 	Vec<Polynomial<GF2>> public_key_perturbed = PublicKey::getPublicKey(matrix_T, vector_T, matrix_S, vector_S, pert_sys_of_polynomials);
-	Vec<GF2> signature_perturbed = Signature :: generateSignaturePerturbed(matrix_T, vector_T, matrix_S, vector_S, hfe, perturbation_polynomials,betas, message, modulus_deg, t);
+	Vec<GF2> signature_perturbed = Signature :: generateSignaturePerturbed(a, matrix_T, vector_T, matrix_S, vector_S, hfe, perturbation_polynomials,betas, message, modulus_deg, t);
 	cout << signature_perturbed << endl;
-	cout << Signature::verifySignature(signature_perturbed, message, public_key_perturbed, modulus_deg) << endl;
+	cout << Signature::verifySignature(a, signature_perturbed, message, public_key_perturbed, modulus_deg) << endl;
 	
+	Vec<GF2> signature_perturbedP = Signature::generateSignaturePerturbedP(a, pert_sys_of_polynomials, matrix_T, vector_T, matrix_S, vector_S, hfe, perturbation_polynomials, betas, message, modulus_deg, t);
+	cout << signature_perturbedP << endl;
+	cout << Signature::verifySignature(a, signature_perturbedP, message, public_key_perturbed, modulus_deg) << endl;
+
 	return 0;
 }
 
